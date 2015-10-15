64ebf5b9fd29869f61d547866302b71a64df7874
Duy Le <dle@walmartlabs.com>
2015-10-15 12:44:02 -0700

    add scroll

--- app/src/main/java/com/training/android/dle/gridimagesearch/activity/SearchActivity.java
+++ app/src/main/java/com/training/android/dle/gridimagesearch/activity/SearchActivity.java
@@ -2,17 +2,20 @@ package com.training.android.dle.gridimagesearch.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.training.android.dle.gridimagesearch.R;
 import com.training.android.dle.gridimagesearch.adapter.ImageResultAdapter;
+import com.training.android.dle.gridimagesearch.listener.EndlessScrollListener;
 import com.training.android.dle.gridimagesearch.model.ImageResult;
 
 import org.json.JSONArray;
@@ -29,15 +32,33 @@ public class SearchActivity extends AppCompatActivity {
     private List<ImageResult> images;
     private ImageResultAdapter imageResultAdapter;
     private GridView gvResult;
+    private static final int VISIBLE_THRESHOLD = 8;
+    private AsyncHttpClient client;
+    private boolean stopLoading = false;
+    private EndlessScrollListener scrollListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        client = new AsyncHttpClient();
         etSearch = (EditText)findViewById(R.id.etSearch);
         images = new ArrayList<ImageResult>();
         imageResultAdapter = new ImageResultAdapter(this, images);
         gvResult = (GridView)findViewById(R.id.gvResult);
         gvResult.setAdapter(imageResultAdapter);
+        scrollListener = new EndlessScrollListener(VISIBLE_THRESHOLD, 0) {
+            @Override
+            public boolean onLoadMore(int page, int totalItemsCount) {
+                if (stopLoading){
+                    return false;
+                }
+                loadImages(page);
+
+                return true;
+            }
+        };
+
+        gvResult.setOnScrollListener(scrollListener);
     }
 
     @Override
@@ -63,20 +84,28 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     public void onSearch(View view) {
+        // reset the variable for scroll listener on a new search
+        scrollListener.reset();
+        loadImages(0);
+    }
+
+    private void loadImages(final int page){
         String text = etSearch.getText().toString();
-        String url = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz=8&q="+text;
-        AsyncHttpClient client = new AsyncHttpClient();
+        int start = page * VISIBLE_THRESHOLD;
+        String url = "https://ajax.googleapis.com/ajax/services/search/images?v=1.0&rsz="+VISIBLE_THRESHOLD+"&q="+text+"&start="+start;
         client.get(url, null, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 JSONArray imageJsonArray = null;
                 try {
+                    stopLoading = false;
                     imageJsonArray = response.getJSONObject("responseData").getJSONArray("results");
-                    images.clear();
-                    images.addAll(ImageResult.toImageResult(imageJsonArray));
-                    imageResultAdapter.notifyDataSetChanged();
+                    if (page == 0){
+                        imageResultAdapter.clear();
+                    }
+                    imageResultAdapter.addAll(ImageResult.toImageResult(imageJsonArray));
                 } catch (JSONException e) {
-                    e.printStackTrace();
+                    stopLoading = true;
                 }
             }
         });
